cc4df1585398bb1a808fe8fd22fa449a3a315b85
Christian Pohl <zapp@zappzarazupp.de>
2017-04-17 12:38:16 +0200

    Ein versuch tabs in lilypond zu konvertieren

---
+++ tab2lily.py
@@ -0,0 +1,15 @@
+#                 0     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15    16    17    18    19 
+a_eis = array (  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'','dis\'\'',  'e\'\'',  'f\'\'','fis\'\'',  'g\'\'','gis\'\'',  'a\'\'','ais\'\'',  'b\'\'')
+a_ees = array (  'e\'',  'f\'','ges\'',  'g\'', 'as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'', 'es\'\'',  'e\'\'',  'f\'\'','ges\'\'',  'g\'\'', 'as\'\'',  'a\'\'','bes\'\'',  'b\'\'')
+a_bis = array (  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'','dis\'\'',  'e\'\'',  'f\'\'','fis\'\'')
+a_bes = array (  'b',  'c\'','des\'',  'd\'',' es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'', 'es\'\'',  'e\'\'',  'f\'\'','ges\'\'')
+a_gis = array (  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'')
+a_ges = array (  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'')
+a_dis = array (  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'')
+a_des = array (  'd',' es',  'e',  'f','ges',  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'')
+a_Ais = array (  'a,','ais,',  'b,',  'c','cis',  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'')
+a_Aes = array (  'a,','bes,',  'b,',  'c','des',  'd', 'es',  'e',  'f','ges',  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'')
+a_Eis = array (  'e,',  'f,','fis,',  'g,','gis,',  'a,','ais,',  'b,',  'c','cis',  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b')
+a_Ees = array (  'e,',  'f,','ges,',  'g,', 'as,',  'a,','bes,',  'b,',  'c','des',  'd', 'es',  'e',  'f','ges',  'g', 'as',  'a','bes',  'b')
+
+
