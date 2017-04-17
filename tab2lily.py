88a69957305b59134691d61d9685d9e825ea5776
Christian Pohl <zapp@zappzarazupp.de>
2017-04-17 13:01:39 +0200

    erst mal wieder python angewoehnen :-D

--- tab2lily.py
+++ tab2lily.py
@@ -1,15 +1,41 @@
-#                 0     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15    16    17    18    19 
-a_eis = array (  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'','dis\'\'',  'e\'\'',  'f\'\'','fis\'\'',  'g\'\'','gis\'\'',  'a\'\'','ais\'\'',  'b\'\'')
-a_ees = array (  'e\'',  'f\'','ges\'',  'g\'', 'as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'', 'es\'\'',  'e\'\'',  'f\'\'','ges\'\'',  'g\'\'', 'as\'\'',  'a\'\'','bes\'\'',  'b\'\'')
-a_bis = array (  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'','dis\'\'',  'e\'\'',  'f\'\'','fis\'\'')
-a_bes = array (  'b',  'c\'','des\'',  'd\'',' es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'', 'es\'\'',  'e\'\'',  'f\'\'','ges\'\'')
-a_gis = array (  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'')
-a_ges = array (  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'')
-a_dis = array (  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'')
-a_des = array (  'd',' es',  'e',  'f','ges',  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'')
-a_Ais = array (  'a,','ais,',  'b,',  'c','cis',  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'')
-a_Aes = array (  'a,','bes,',  'b,',  'c','des',  'd', 'es',  'e',  'f','ges',  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'')
-a_Eis = array (  'e,',  'f,','fis,',  'g,','gis,',  'a,','ais,',  'b,',  'c','cis',  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b')
-a_Ees = array (  'e,',  'f,','ges,',  'g,', 'as,',  'a,','bes,',  'b,',  'c','des',  'd', 'es',  'e',  'f','ges',  'g', 'as',  'a','bes',  'b')
+# nummer auf Tab --> lily
+# achtung: absoulte oktavbezeichnung
+# tabs muessen wie folgt aufgebaut sein:
+
+# e--
+# b--
+# g--
+# d--
+# A--
+# E--
+# d.h. Saite und -- am Begin der Zeile
+
+import re
+
+a_eis = ['e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'','dis\'\'',  'e\'\'',  'f\'\'','fis\'\'',  'g\'\'','gis\'\'',  'a\'\'','ais\'\'',  'b\'\'']
+a_ees = ['e\'',  'f\'','ges\'',  'g\'', 'as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'', 'es\'\'',  'e\'\'',  'f\'\'','ges\'\'',  'g\'\'', 'as\'\'',  'a\'\'','bes\'\'',  'b\'\'']
+a_bis = ['b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'','dis\'\'',  'e\'\'',  'f\'\'','fis\'\'']
+a_bes = ['b',  'c\'','des\'',  'd\'',' es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'', 'es\'\'',  'e\'\'',  'f\'\'','ges\'\'']
+a_gis = ['g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'','ais\'',  'b\'',  'c\'\'','cis\'\'',  'd\'\'']
+a_ges = ['g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'','bes\'',  'b\'',  'c\'\'','des\'\'',  'd\'\'']
+a_dis = ['d','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'',  'f\'','fis\'',  'g\'','gis\'',  'a\'']
+a_des = ['d',' es',  'e',  'f','ges',  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'',  'f\'','ges\'',  'g\'',' as\'',  'a\'']
+a_Ais = ['a,','ais,',  'b,',  'c','cis',  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b',  'c\'','cis\'',  'd\'','dis\'',  'e\'']
+a_Aes = ['a,','bes,',  'b,',  'c','des',  'd', 'es',  'e',  'f','ges',  'g',' as',  'a','bes',  'b',  'c\'','des\'',  'd\'', 'es\'',  'e\'']
+a_Eis = ['e,',  'f,','fis,',  'g,','gis,',  'a,','ais,',  'b,',  'c','cis',  'd','dis',  'e',  'f','fis',  'g','gis',  'a','ais',  'b']
+a_Ees = ['e,',  'f,','ges,',  'g,', 'as,',  'a,','bes,',  'b,',  'c','des',  'd', 'es',  'e',  'f','ges',  'g', 'as',  'a','bes',  'b']
+
+
+o_file = open("testtab.txt","r")
+i=0
+for s_line in o_file:
+    i += 1
+    print 'Parsing line ',i,':',s_line
+
+    if(re.match('^e--',s_line)):
+        print "e found - read all strings"
+
+
+o_file.close()
 
 
